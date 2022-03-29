f25cac2ba25b860e1d4a132be542a8a4ef19257b
kobashihiroaki <h.kobashi@g-waters.com>
2022-03-29 21:15:28 +0900

    バグ修正

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -34,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
         historyTextView = findViewById(R.id.history);
         //最大値の初期値をEditTextにセットする
         maxNumberEditText.setText("" + maxNumber);
+        //１から最大値まで配列で表示する
+        for (int i = 1; i <= maxNumber; i++) {
+            history.add(String.valueOf(i));
+        }
+        historyTextView.setText(history.toString());
+
         //最大値を更新する
         registerMaxNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +49,15 @@ public class MainActivity extends AppCompatActivity {
                 //int型に変換してから代入する
                 maxNumber = Integer.valueOf(maxNumberString);
                 Log.d("MainActivity", "maxNumber:" + maxNumber);
+                //配列をすべて消去
+                history.clear();
+                //1から最大値まで配列で表示する
+                for (int i = 1; i <= maxNumber; i++) {
+                    history.add(String.valueOf(i));
+                }
+                historyTextView.setText(history.toString());
+                //nextNumberを空にして表示する
+                currentNumberTextView.setText("");
             }
         });
         //表示中の数字を更新する
@@ -57,30 +72,37 @@ public class MainActivity extends AppCompatActivity {
     //next_numberのボタンがタップされたときの処理
     private void onClickNextNumber() {
         Log.d("MainActivity", "onClickNextNumber");
+        //配列が空ならビンゴ終了、空でなければ数字を表示
+        if (history.size() == 0) {
+            currentNumberTextView.setText("出尽くし");
+        } else {
+            //maxNumberを考慮したランダムな数値
+            int nextNumber = createRandomNumber();
 
-        //maxNumberを考慮したランダムな数値
-        int nextNumber = createRandomNumber();
+            //重複している数値だった場合は、数値の生成をやり直す
+            //while (history.contains("" + nextNumber)) {
+            //Log.d("MainActivity", "重複したので再生成");
+            //nextNumber = createRandomNumber();
+            //}
+            //nextNumberを生成
+            //nextNumber = createRandomNumber();
 
-        //重複している数値だった場合は、数値の生成をやり直す
-        while (history.contains("" + nextNumber)) {
-            Log.d("MainActivity", "重複したので再生成");
-            nextNumber = createRandomNumber();
+            //nextNumberを文字列に変換する
+            String nextNumberStr = "" + nextNumber;
+
+            //nextNumberを画面に表示する
+            currentNumberTextView.setText(nextNumberStr);
+            //履歴を残す
+            //history.add(nextNumberStr);
+            //配列から生成された数字を消す
+            history.remove(history.indexOf(nextNumberStr));
+            //履歴を表示する
+            historyTextView.setText(history.toString());
         }
-
-        //nextNumberを文字列に変換する
-        String nextNumberStr = "" + nextNumber;
-
-        //nextNumberを画面に表示する
-        currentNumberTextView.setText(nextNumberStr);
-
-        //履歴を残す
-        history.add(nextNumberStr);
-
-        //履歴を表示する
-        historyTextView.setText(history.toString());
     }
     private int createRandomNumber() {
-        double randomNumber = Math.random() * (maxNumber - 1);
-        return (int) randomNumber + 1;
+        double historyNumber = Math.random() * history.size();
+        int randomNumber = Integer.parseInt(history.get((int)historyNumber));
+        return randomNumber;
     }
 }
